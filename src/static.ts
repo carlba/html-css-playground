f113710a5cda4d02b8f62fddf8121a07d39998f6
Carl Bäckström <genzorg@gmail.com>
2021-01-23 21:51:47 +0100

    Fix many things

--- src/static.ts
+++ src/static.ts
@@ -4,46 +4,50 @@ import * as fs from 'fs';
 import * as MarkdownIt from 'markdown-it';
 import * as expressHandlebars from 'express-handlebars';
 
-const app = express();
-const port = 3030;
 const md = new MarkdownIt();
+const port = 3030;
 const applicationPath = __dirname;
 const staticFolder = path.join(applicationPath, 'static');
-app.set('views', path.join(applicationPath, 'views'));
-app.use('/static', express.static(staticFolder));
-app.use(express.static(path.join(__dirname, '/public')));
-
-app.engine('handlebars', expressHandlebars());
-app.set('view engine', 'handlebars');
 
 function readMarkdownFile(filepath: string): string {
   return md.render(fs.readFileSync(filepath).toString());
 }
 
-app.use((req, res) => {
-  res.setHeader('content-type', 'text/html');
-  fs.readdir(staticFolder, (err, files) => {
-    const readmes = files
-      .map(file => {
-        const filepath = path.join(staticFolder, file);
-        const readmeFilepath = path.join(filepath, 'README.md');
-        if (
-          fs.lstatSync(filepath).isDirectory() &&
-          fs.existsSync(readmeFilepath)
-        ) {
-          return {
-            content: readMarkdownFile(readmeFilepath),
-            src: `static/${file}`,
-            title: file,
-          };
-        } else {
-          return null;
-        }
-      })
-      .filter(readme => readme);
-    res.render('home', {readmes, layout: false});
+function isValidPath(filepath: string) {
+  return fs.lstatSync(filepath).isDirectory() && !filepath.startsWith('.');
+}
+
+function createApp(port: number, staticFolder: string) {
+  const app = express();
+  app.set('views', path.join(applicationPath, 'views'));
+  app.use('/static', express.static(staticFolder));
+  app.use(express.static(path.join(__dirname, '/public')));
+
+  app.engine('handlebars', expressHandlebars());
+  app.set('view engine', 'handlebars');
+
+  app.use((req, res) => {
+    res.setHeader('content-type', 'text/html');
+    fs.readdir(staticFolder, (err, files) => {
+      const projects = files
+        .map(file => {
+          const filepath = path.join(staticFolder, file);
+          const readmeFilepath = path.join(filepath, 'README.md');
+          if (isValidPath(filepath)) {
+            const content = fs.existsSync(readmeFilepath) ? readMarkdownFile(readmeFilepath) : 'No Content';
+            return { content, src: `static/${file}`, title: file };
+          } else {
+            return null;
+          }
+        })
+        .filter(project => project);
+      res.render('home', { projects, layout: false });
+    });
   });
-});
+  return app;
+}
+
+const app = createApp(port, staticFolder);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
